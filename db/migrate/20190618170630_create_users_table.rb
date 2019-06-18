b1c522e3356aba80a3a4e967b75c8a92353b1551
Anthony Rurup <tcrurup@gmail.com>
2019-06-18 17:08:09 +0000

    Automatically backed up by Learn

--- db/migrate/20190618170630_create_users_table.rb
+++ db/migrate/20190618170630_create_users_table.rb
@@ -1,7 +1,9 @@
 class CreateUsersTable < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string 
+      t.string :name
+      t.string :email
+      t.string :password
     end
   end
 end
